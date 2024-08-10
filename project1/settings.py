b094caa91a8e42595ac840e8a3527559c766d918
Isaque Anderson <isaqanderson23@gmail.com>
2024-08-10 13:58:47 -0300

    timezone error

--- project1/settings.py
+++ project1/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = ['*']
 
 
 # Application definition
